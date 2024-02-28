7911dffb1026f9b81a5717a1c95df808733194a4
ashpatx <thingslearn3@gmail.com>
2024-02-28 16:31:14 -0500

    gradient setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { Gradient } from "./assets/gradient";
+
+export default function MyComponent() {
+    useEffect(() => {
+        const gradient = new Gradient();
+        gradient.initGradient("#gradient-canvas");
+    }, []);
+
+    return (
+      <main className="min-h-screen flex flex-col relative bg-slate-900">
+      <div className="relative z-10 flex flex-col flex-1 justify-center max-w-6xl p-10">
+         <h1 className="text-7xl font-bold text-white">
+            Woah, look at this whatamesh gradient.
+         </h1>
+      </div>
+      <canvas
+         id="gradient-canvas"
+         className="fixed inset-0"
+         data-transition-in
+         />
+   </main>
+    );
+}

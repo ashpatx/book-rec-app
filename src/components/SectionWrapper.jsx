05711f6e526b5006431a86fe83731505002e7156
ashpatx <thingslearn3@gmail.com>
2024-03-03 16:59:15 -0500

    created books dataset

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,9 +4,9 @@ export default function SectionWrapper(props) {
     const {children, header, title} = props
   return (
     <section className='min-h-screen flex flex-col'>
-        <div className='bg-slate-900 py-10 flex flex-col gap-4 justify-center items-center'>
+        <div className='py-10 flex flex-col gap-4 justify-center items-center'>
             <p>{header}</p>
-            <h2>{title[0]} <span>{title[1]}</span> {title[2]}</h2>
+            <h2>{title[0]} <span>{title[1]}</span> <span>{title[2]}</span> <span>{title[3]}</span></h2>
         </div>
         {children}
     </section>

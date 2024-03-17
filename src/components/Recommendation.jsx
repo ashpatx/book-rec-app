ec98d2879e7086cb1fd229372695100d0e0d0e1f
ashpatx <thingslearn3@gmail.com>
2024-03-17 12:21:36 -0400

    Update Recommendation.jsx

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -6,7 +6,7 @@ export default function Recommendation(props) {
   const { recommendation } = props;
 
   return (
-    <SectionWrapper id={'recommendation'} header={"Explore New Reads"} title={['We', 'Recommend:']}>
+    <SectionWrapper id={'recommendation'} header={"Explore New Reads"} title={['Try:']}>
       <div className='flex flex-col gap-4'>
         {recommendation.map((book, i) => (
           <BookCard key={i} book={book} />
